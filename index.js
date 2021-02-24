5ecb27aceb88203cbd98abf883c2685f208cd093
Shadowstrorm10 <75273905+Shadowstrorm10@users.noreply.github.com>
2021-02-24 00:37:47 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-const Discord = require("discord.js");
+require("dotenv").config();//Loading .env
 const fs = require("fs");
+const { Collection, Client } = require("discord.js");
 
-
-const client = new Discord.Client();
-const config = require("./config.js");
-client.config = config;
+const client = new Client();//Making a discord bot client
+client.commands = new Collection();//Making client.commands as a Discord.js Collection
 client.queue = new Map()
 
-fs.readdir("./events/", (err, files) => {
+client.config = {
+  prefix: process.env.PREFIX
+}
+
+//Loading Events
+fs.readdir(__dirname + "/events/", (err, files) => {
   if (err) return console.error(err);
-  files.forEach(file => {
-    const event = require(`./events/${file}`);
+  files.forEach((file) => {
+    const event = require(__dirname + `/events/${file}`);
     let eventName = file.split(".")[0];
     client.on(eventName, event.bind(null, client));
+    console.log("Loading Event: "+eventName)
   });
 });
 
-client.commands = new Discord.Collection()
-
+//Loading Commands
 fs.readdir("./commands/", (err, files) => {
   if (err) return console.error(err);
-  files.forEach(file => {
+  files.forEach((file) => {
     if (!file.endsWith(".js")) return;
     let props = require(`./commands/${file}`);
     let commandName = file.split(".")[0];
-    console.log(`Attempting to load command ${commandName}`);
     client.commands.set(commandName, props);
+    console.log("Loading Command: "+commandName)
   });
 });
 
-client.login(config.token);
+//Logging in to discord
+client.login(process.env.TOKEN)
